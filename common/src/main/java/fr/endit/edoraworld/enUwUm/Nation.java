4465697ebd56e039e5b1f35efc2d0f861e9e1e7c
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2024-01-21 01:00:40 +0100

    Add Nation enum

---
+++ common/src/main/java/fr/endit/edoraworld/enUwUm/Nation.java
@@ -0,0 +1,20 @@
+package fr.endit.edoraworld.enUwUm;
+
+import lombok.*;
+import net.minecraft.network.chat.Component;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum Nation {
+    Kawan("Kawan", "Loc'na'ko", "Dakara", Component.translatable("nations.edoraworld.kawan.description"));
+    /*
+     * Othala("Othala"),
+     * Druag("Druag"),
+     * Hajim("Hajim");
+     */
+
+    private final String name;
+    private final String origin;
+    private final String capital;
+    private final Component description;
+}
